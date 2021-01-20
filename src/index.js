381d938dea7b3377bd8cab6e9d3b57dc479f685d
Aruga <aruga17z@gmail.com>
2021-01-20 22:21:40 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const fetch = require('node-fetch');
 const ytb = require('../lib/youtube')
 const baseURI = 'https://arugaz.my.id'
-var ArugaZ = module.exports;
 
-ArugaZ.ytsearch = function ytsearch(query) {
+module.exports.ytsearch = function ytsearch(query) {
 	return new Promise((resolve, reject) => {
 		fetch(baseURI+'/api/media/ytsearch?query=' + encodeURIComponent(query))
 		.then(res => res.json())
@@ -12,7 +11,7 @@ ArugaZ.ytsearch = function ytsearch(query) {
 	})
 }
 
-ArugaZ.ytaudio = function ytaudio(url) {
+module.exports.ytaudio = function ytaudio(url) {
 	return new Promise((resolve, reject) => {
 		ytb.yta(url)
 		.then(resolve)
@@ -20,10 +19,64 @@ ArugaZ.ytaudio = function ytaudio(url) {
 	})
 }
 
-ArugaZ.ytvideo = function ytsearch(url) {
+module.exports.ytvideo = function ytsearch(url) {
 	return new Promise((resolve, reject) => {
 		ytb.ytv(url)
 		.then(resolve)
 		.catch(reject)
+	})
+}
+
+module.exports.instagram = function instagram(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/media/ig?url=' + query)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.stalkig = function stalkig(query) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/media/stalkig?user=' + encodeURIComponent(query))
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.storyig = function storyig(query) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/media/storyig?user=' + encodeURIComponent(query))
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.highlightig = function highlightig(query) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/media/highlightig?user=' + encodeURIComponent(query))
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.tiktok = function tiktok(url) {
+	return new Promise((resolve, reject) => {
+		fetch(baseURI+'/api/media/tiktok?user=' + url)
+		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+module.exports.youtube = function(url, options) {
+	options = {
+		k: String
+	}
+	return new Promise((resolve, reject) => {
+		if (!options.k) return reject('PLEASE INPUT Option')
 	})
 }
