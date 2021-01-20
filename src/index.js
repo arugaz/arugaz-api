64aece6d40736574f43f67f728e80cd2a471dde7
Aruga <aruga17z@gmail.com>
2021-01-20 19:52:36 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const fetch = require('node-fetch');
+const ytb = require('./lib/youtube')
 const baseURI = 'https://arugaz.my.id'
 var ArugaZ = module.exports;
 
-ArugaZ.ytsearch = function ytsearch(query, cb) {
+ArugaZ.ytsearch = function ytsearch(query) {
 	return new Promise((resolve, reject) => {
 		fetch(baseURI+'/api/media/ytsearch?query=' + encodeURIComponent(query))
 		.then(res => res.json())
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+ArugaZ.ytaudio = function ytaudio(url) {
+	return new Promise((resolve, reject) => {
+		ytb.yta(url)
+		.then(resolve)
+		.catch(reject)
+	})
+}
+
+ArugaZ.ytvideo = function ytsearch(url) {
+	return new Promise((resolve, reject) => {
+		ytb.ytv(url)
 		.then(resolve)
 		.catch(reject)
 	})
